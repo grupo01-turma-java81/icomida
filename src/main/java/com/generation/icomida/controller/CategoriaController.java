5445bd1ddbdbabd894a7320cae2dd0c92cbbd231
Yasmim <yasmim.ruescas@outlook.com>
2025-04-25 13:06:19 -0300

    feat: Criação das Classes CategoriaController e CategoriaRepository

--- src/main/java/com/generation/icomida/controller/CategoriaController.java
+++ src/main/java/com/generation/icomida/controller/CategoriaController.java
@@ -1,5 +1,59 @@
 package com.generation.icomida.controller;
 
+import com.generation.icomida.model.Categoria;
+import com.generation.icomida.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import jakarta.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/categorias")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CategoriaController {
 
+	@Autowired
+	private final CategoriaRepository categoriaRepository;
+
+	public CategoriaController(CategoriaRepository categoriaRepository) {
+		this.categoriaRepository = categoriaRepository;
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Categoria>> getAll() {
+		return ResponseEntity.ok(categoriaRepository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> getById(@PathVariable Long id) {
+		return categoriaRepository.findById(id).map(categoria -> ResponseEntity.ok(categoria))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> post(@Valid @RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
+	}
+
+	@PutMapping
+	public ResponseEntity<Categoria> put(@Valid @RequestBody Categoria categoria) {
+		if (categoriaRepository.existsById(categoria.getId())) {
+			return ResponseEntity.ok(categoriaRepository.save(categoria));
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Categoria> categoria = categoriaRepository.findById(id);
+		if (categoria.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		categoriaRepository.deleteById(id);
+	}
 }
--- src/main/java/com/generation/icomida/repository/CategoriaRepository.java
+++ src/main/java/com/generation/icomida/repository/CategoriaRepository.java
@@ -1,5 +1,13 @@
 package com.generation.icomida.repository;
 
-public class CategoriaRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.icomida.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+	public List<Categoria> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
