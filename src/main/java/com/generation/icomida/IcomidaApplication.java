b5c9b96782d5743bef62ae66b53ac222977741d7
Vitoriacmlly <vcamilly18@gmail.com>
2025-04-25 11:13:54 -0300

    projeto

---
+++ src/main/java/com/generation/icomida/IcomidaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.icomida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcomidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcomidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/icomida/IcomidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.icomida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcomidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
