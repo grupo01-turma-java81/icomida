332371191eaeca52a388429ed9562bd19598d399
GiulioArantes <giulio.arantes@icloud.com>
2025-06-05 09:25:36 -0300

    part2: incluido crossorigin em produtocontroller

--- src/main/java/com/generation/icomida/controller/ProdutoController.java
+++ src/main/java/com/generation/icomida/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,46 +25,44 @@ import jakarta.validation.Valid;
 @RequestMapping("/produto")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdutoController {
-    
-    
-    
+
     @Autowired
     private ProdutoService produtoService;
 
     @GetMapping
     public ResponseEntity<List<Produto>> getAll() {
-	return ResponseEntity.ok(produtoService.listaTodosProdutos());
+        return ResponseEntity.ok(produtoService.listaTodosProdutos());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Produto> getById(@PathVariable Long id) {
-	return ResponseEntity.ok().body(produtoService.buscarPorId(id));
+        return ResponseEntity.ok().body(produtoService.buscarPorId(id));
     }
 
     @GetMapping("/nome/{nome}")
     public ResponseEntity<List<Produto>> getByNome(@PathVariable String nome) {
-	return ResponseEntity.ok(produtoService.buscarPorNome(nome));
+        return ResponseEntity.ok(produtoService.buscarPorNome(nome));
     }
-    
+
     @GetMapping("/produtosaudavel/{saudavel}")
     public ResponseEntity<List<Produto>> getProdutoSaudavel(@PathVariable String saudavel) {
-	return ResponseEntity.ok(produtoService.recomendacaoDeProdutoSaudavel(saudavel));
+        return ResponseEntity.ok(produtoService.recomendacaoDeProdutoSaudavel(saudavel));
     }
 
     @PostMapping
     public ResponseEntity<Produto> postProduto(@Valid @RequestBody Produto produto) {
-	return produtoService.cadastrarProduto(produto);
+        return produtoService.cadastrarProduto(produto);
     }
 
     @PutMapping
     public ResponseEntity<Produto> putProduto(@Valid @RequestBody Produto produto) {
-	return produtoService.atualizarProduto(produto);
+        return produtoService.atualizarProduto(produto);
     }
-    
+
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
-	produtoService.deletarProduto(id);
+        produtoService.deletarProduto(id);
     }
-    
+
 }
